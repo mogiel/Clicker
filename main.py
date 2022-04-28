01fc65d23f57863853c225bd985f3b6d374da96f
mogiel <mogielski@protonmail.com>
2022-04-28 21:40:37 +0200

    0.1.5 - adding docstring and types

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+"""module for automating tasks using external scripts. Single and multiple scripts"""
+# system module
 from json import load
-from pynput.keyboard import Key, Controller as Controller_Keyboard
-from pynput.mouse import Button, Controller as Controller_Mouse
 from random import uniform
 from time import sleep
+from typing import TypedDict, Optional, Callable
+# external module
+from typing import Dict
 
-keyboard_keys = [
-    'alt', 'backspace', 'cmd', 'ctrl', 'delete', 'down', 'end', 'enter',
-    'esc', 'f1', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18',
-    'f19', 'f2', 'f20', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'home',
-    'insert', 'left', 'menu', 'pause', 'right', 'shift', 'space', 'tab', 'up'
-]
+from pynput.keyboard import Key, Controller as Controller_Keyboard
+from pynput.mouse import Button, Controller as Controller_Mouse
 
-mouse_button = ['left', 'right', 'middle', 'x1', 'x2']
+# all keyboard key for validation
+keyboard_keys = [e.name for e in Key]
+# all mouse button for validation
+mouse_button: list[str] = [e.name for e in Button]
+
+
+class SetMouse(TypedDict):
+    x: int
+    y: int
+
+
+class MoveMouse(TypedDict):
+    dx: int
+    dy: int
+    click: Optional[str]
+
+
+class ScriptException(Exception):
+    pass
 
 
 class ScriptsRobot:
-    def __init__(self, script_list: [str]):
+    """Automate your mouse and keyboard by passing action scripts from a json file"""
+    def __init__(self, script_list: list[str]):
+        """
+        :param script_list: list of filenames with scripts
+        :type script_list: [str,...]
+        :rtype: object
+        """
         self.script_list = script_list
         self.keyboard = Controller_Keyboard()
         self.mouse = Controller_Mouse()
@@ -28,23 +51,45 @@ class ScriptsRobot:
             'Press mouse button': self.press_mouse_button,
             'Double click mouse button': self.double_click_mouse_button,
             'Move mouse': self.move_mouse,
+            'Press and move': self.press_and_move,
             'Scroll': self.scroll,
         }
-        self.script = self.open_script()
+        self.scripts = self.open_script()
+        self.script_correct = True
 
     @staticmethod
-    def valid_key(key):
+    def valid_key(key: str):
+        """validator if the key is correct
+
+        :param: name of key
+        :rtype: str
+        """
         if key in keyboard_keys:
-            return True
+            return key
+        elif len(key) == 1:
+            return key
 
     @staticmethod
     def wait(wait: float = .25):
+        """random stop script
+
+        :type wait: float
+        """
         sleep(uniform(0.75 * wait, 1.5 * wait))
 
+    def script_stop(self):
+        """script termination in case of error
+
+        :rtype: bool"""
+        self.script_correct = False
+
     def open_script(self):
+        """opening a .json file with a script. write activities to the list
+
+        :rtype: [object]"""
         script = []
         for script_name in self.script_list:
-            with open(f'scripts/{script_name}.json') as file:
+            with open(f'scripts/{script_name}.json', encoding='utf-8') as file:
                 data = load(file)
                 for entity in data:
                     for item in entity.items():
@@ -52,41 +97,99 @@ class ScriptsRobot:
 
         return script
 
-    def pressed_keys(self, keys_list):
-        first_key, second_key = keys_list
-        key = getattr(Key, second_key) if len(second_key) > 1 else second_key
-        with self.keyboard.pressed(getattr(Key, first_key, 'esc')):
-            self.keyboard.press(key)
-            self.keyboard.release(key)
+    def pressed_keys(self, keys: list[str]):
+        """pressing two keys
 
-    def press_key(self, key):
-        new_key = getattr(Key, key) if len(key) > 1 else key
-        self.keyboard.press(new_key)
-        self.keyboard.release(new_key)
+        :type keys: list[str]
+        """
+        first_key, second_key = keys
+        try:
+            first_key = self.valid_key(first_key)
+            second_key = self.valid_key(second_key)
+            key = getattr(Key, second_key) if len(second_key) > 1 else second_key
+            with self.keyboard.pressed(getattr(Key, first_key, 'esc')):
+                self.keyboard.press(key)
+                self.keyboard.release(key)
+        except TypeError:
+            self.script_stop()
+            print("Script error")
 
-    def set_text(self, text):
-        self.keyboard.type(text)
+    def press_key(self, key: list[keyboard_keys] | str):
+        """pressing one key"""
+        try:
+            new_key = getattr(Key, key) if len(key) > 1 else key
+            self.keyboard.press(new_key)
+            self.keyboard.release(new_key)
+        except TypeError:
+            self.script_stop()
+            print("Script error")
 
-    def set_mouse_position(self, coordinates):
+    def set_text(self, text: str):
+        """text introduction
+
+        :type text: str
+        """
+        new_list = list(text)
+        while new_list:
+            self.keyboard.type(new_list.pop(0))
+            self.wait(0.05)
+
+    def set_mouse_position(self, coordinates: SetMouse):
+        """set the position of the mouse
+
+        :param coordinates: {"x": int, "y": int}
+        :type coordinates: dict
+        """
         self.mouse.position = (coordinates['x'], coordinates['y'])
 
-    def press_mouse_button(self, click):
+    def press_mouse_button(self, click: mouse_button):
+        """pressing the mouse button
+
+        :param click: mou """
         self.mouse.press(getattr(Button, click, 'left'))
         self.mouse.release(getattr(Button, click, 'left'))
 
-    def move_mouse(self, coordinates):
+    def move_mouse(self, coordinates: MoveMouse):
+        """move the mouse
+
+        :param coordinates: {"dx": int, "dy": int}
+        """
         self.mouse.move(coordinates['dx'], coordinates['dy'])
 
-    def double_click_mouse_button(self, click):
+    def double_click_mouse_button(self, click: mouse_button):
+        """double click the mouse
+
+        :param click: mouse_button
+        :return:
+        """
         self.mouse.click(getattr(Button, click, 'left'), 2)
 
     def scroll(self, steps: int):
+        """scroll mouse
+
+        :type steps: int
+        """
         self.mouse.scroll(0, steps)
 
+    def press_and_move(self, action: MoveMouse):
+        """press the button and move
+
+        :param action: {"dx": int, "dy": int, "click": mouse_button}
+        :type action: MoveMouse
+        """
+        self.mouse.press(getattr(Button, action['click'], 'left'))
+        self.mouse.move(action['dx'], action['dy'])
+        self.mouse.release(getattr(Button, action['click'], 'left'))
+
     def action(self):
-        for i in self.script:
-            self.action_mapper.get(i[0])(i[1])
+        """run the script"""
+        for script in self.scripts:
+            if self.script_correct:
+                self.action_mapper.get(script[0])(script[1])
 
 
-test_class = ScriptsRobot(["Open_Chrome", "Pajacyk"])
-test_class.action()
+if __name__ == "__main__":
+    test_class = ScriptsRobot(["Open_Word"])
+    test_class.action()
+
+
--- testy.py
+++ testy.py
@@ -1,59 +1,20 @@
-from pynput.keyboard import Key, Controller
-from time import sleep
-
-keyboard = Controller()
-
-# keyboard
-
+# import timeit
 #
-# with keyboard.pressed(Key.cmd):
-#     keyboard.press('s')
-#     keyboard.release('s')
+# print(timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
 #
+# print(timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000))
 #
-# sleep(1)
-#
-# keyboard.type('Google chrome')
-# sleep(0.5)
-# keyboard.press(Key.enter)
-# keyboard.release(Key.enter)
-# sleep(1)
-# keyboard.type('unreal3')
-# sleep(1)
-# keyboard.press(Key.enter)
-# keyboard.release(Key.enter)
+# print(timeit.timeit('"-".join(map(str, range(100)))', number=10000))
 
 
-# from selenium import webdriver
-# from selenium.webdriver.support import expected_conditions
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-#
-# browser = webdriver.Chrome()
-# browser.get('http://google.com/')
-#
-# with keyboard.pressed(Key.cmd):
-#     keyboard.press(Key.up)
-#     keyboard.release(Key.up)
-#
-# posts_loaded = expected_conditions.presence_of_element_located((By.ID, 'L2AGLb'))
-# WebDriverWait(browser, 5).until(posts_loaded)
-# posts_loaded.click()
-
-# browser.stop_client()
-# sleep(2)
-# browser.close()
+action = {
+    "click": "left",
+    "dx": 123,
+    "dy": 231,
+}
 
 
-# value1 = 3
-# value2 = 4
-# action = 'dodaj a + b'
-#
-# action_mapper = {
-#     'dodaj a + b': lambda a, b: a + b,
-#     'odejmij': lambda a, b: a - b,
-#     'pomnóż': lambda a, b: a * b,
-#     'podziel': lambda a, b: a / b,
-# }
-#
-# print(action_mapper.get(action, 'dodaj')(value1, value2))
+def test(**kwargs):
+    print(kwargs['dy'])
+
+test(**action)
